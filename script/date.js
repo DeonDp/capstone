a0f86c1a001a335e17793283e1facf74453c2508
DeonDp <deongohard@hotmail.com>
2017-05-26 15:33:15 +0000

    Updated

--- script/date.js
+++
@@ -1,7 +0,0 @@
-window.onload =function(){
-	var dayName = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-	var monName = new Array ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-	var now = new Date;
-	var today =("" + dayName[now.getDay()] + ", " + monName[now.getMonth()] + " "+now.getDate() +".") 
-	document.getElementById("date").innerHTML=today;
-};
